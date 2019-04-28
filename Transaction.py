91fe5b314a84e78ad7466b7bba9df0bd334db0a5
Dhruvik <navadiyadhruvik2@gmail.com>
2019-04-28 16:42:52 +0530

    blockchain validation added

--- Transaction.py
+++ Transaction.py
@@ -1,9 +1,7 @@
+#Transaction.py
 import Signetures
+
 class Tx:
-	input = None
-	output =  None
-	sigs = None
-	reqd = None
 
 	def __init__(self):
 		self.input=[]
@@ -17,27 +15,56 @@ class Tx:
 	def add_output(self,to_addr,amount):
 		self.output.append((to_addr,amount))
 
-	def add_reqd(Self,addr):
-		self.reqd.append(addr)
-
-	def sign(self,private):
+	def sign(self,private_key):
 		message = self.__gather()
-		newsig = Signetures.sign(message,private)
+		newsig = Signetures.sign(message,private_key)
 		self.sigs.append(newsig)
 
 	def is_valid(self):
+		total_in=0
+		total_out=0
+
+
+
 		message = self.__gather()
-		for addr,amout in self.input:
+		for addr,amount in self.input:
 			found = False
+
+			total_in+=amount
 
 			for s in self.sigs:
 				if Signetures.verify(message,s,addr):
 					found=True
 
-				if not found:
-					return False
+			if not found:
+				return False
+
+			if amount<0:
+				return False
+
+		for addr in self.reqd:
+			found = False
+
+			for s in self.sigs:
+				if Signetures.verify(message,s,addr):
+					found=True
+			if not found:
+				return False
+
+		for addr,amount in self.output:
+			total_out+=amount
+			if amount <0:
+				return False
+
+		if total_out > total_in:
+			return False
 
 		return True
+
+
+
+	def add_reqd(self,private_key):
+		self.reqd.append(private_key)
 
 	def __gather(self):
 		data = []
@@ -47,8 +74,7 @@ class Tx:
 
 		return data
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
 	pr1,pu1 = Signetures.generate_keys()
 	pr2,pu2 = Signetures.generate_keys()
 	pr3,pu3 = Signetures.generate_keys()
@@ -65,30 +91,66 @@ if __name__=='__main__':
 	tx2.add_output(pu3,1)
 	tx2.sign(pr1)
 
+
 	tx3 = Tx()
 	tx3.add_input(pu3,1.2)
-	tx3.add_output(pu3,1.1)
+	tx3.add_output(pu1,1.1)
+	tx3.add_reqd(pu4)
 	tx3.sign(pr3)
+	tx3.sign(pr4)
+
 
 	for t in [tx1,tx2,tx3]:
-		print(t,t.is_valid())
+		if t.is_valid():
+			print("Success! transacion is valid")
+		else:
+			print("Error ! transacion is not valid")
 
-	tx3 = Tx()
-	tx3.add_input(pu3,1.2)
-	tx3.add_output(pu3,1.1)
-	tx3.sign(pr3)
 
-	tx3 = Tx()
-	tx3.add_input(pu3,1.2)
-	tx3.add_output(pu3,1.1)
-	tx3.sign(pr3)
+	tx4 = Tx()
+	tx4.add_input(pu1,1)
+	tx4.add_output(pu2,1)
+	tx4.sign(pr2)
 
-	tx3 = Tx()
-	tx3.add_input(pu3,1.2)
-	tx3.add_output(pu3,1.1)
-	tx3.sign(pr3)
+	#escrow not sign by the arbiter
+	tx5 = Tx()
+	tx5.add_input(pu3,1.2)
+	tx5.add_output(pu1,1.1)
+	tx5.add_reqd(pu4)
+	tx5.sign(pr3)
 
-	tx3 = Tx()
-	tx3.add_input(pu3,1.2)
-	tx3.add_output(pu3,1.1)
-	tx3.sign(pr3)
+	tx6 = Tx()
+	tx6.add_input(pu3,1)
+	tx6.add_input(pu4,0.1)
+	tx6.add_output(pu1,1.1)
+	tx6.sign(pr3)
+
+	tx7 = Tx()
+	tx7.add_input(pu4,1.2)
+	tx7.add_output(pu1,1)
+	tx7.add_output(pu2,2)
+	tx7.sign(pr4)
+
+	tx8 = Tx()
+	tx8.add_input(pu2,-1)
+	tx8.add_output(pu1,-1)
+	tx8.sign(pr2)
+
+
+	for t in [tx4,tx5,tx6,tx7,tx8]:
+		if t.is_valid():
+			print("Error ! is valid")
+		else:
+			print("Suc !  invalid")
+
+
+
+
+
+
+
+
+
+
+
+	
